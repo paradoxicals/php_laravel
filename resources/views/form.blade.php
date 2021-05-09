41092060c31241d91855e75d669329d3f64e0bb8
Serkan Danışan <serkan.danisan@gmail.com>
2021-05-09 17:55:03 +0300

    Form examples

---
+++ resources/views/form.blade.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <form action="{{route('postIt')}}">
+      <textarea name="text" style="width:300px; height:200px;"></textarea><br>
+      <input type="submit" name"send" value="Send">
+    </form>
+  </body>
+</html>
