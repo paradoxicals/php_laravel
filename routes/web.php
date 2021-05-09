0b5e8af870d4a5182c92ff0dd7d9a37a26be6473
Serkan Danışan <serkan.danisan@gmail.com>
2021-05-09 19:03:35 +0300

    Form and middleware operations

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Example;
 use App\Http\Controllers\Web;
 use App\Http\Controllers\Homepage;
+use App\Http\Controllers\Forms;
+use App\Http\Controllers\DbOperations;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +20,12 @@ Route::get('/poem/{author}',[Example::class, 'lookUp']);
 Route::get('/web',[Web::class,'initSite'])->name('self');
 
 Route::get('/home',[Homepage::class,'initSite'])->name('home');
+
+Route::get('/form',[Forms::class,'postForm']);
+
+Route::middleware('check')->get('/formResult',[Forms::class,'postIt'])->name('postIt');
+
+Route::get('/add',[DbOperations::class,'add']);
+Route::get('/update',[DbOperations::class,'update']);
+Route::get('/delete',[DbOperations::class,'delete']);
+Route::get('/list',[DbOperations::class,'getRecords']);
